554285a981943c4253e53a098b972d46a7082fe6
Anmol Goel <mrgoel2975@gmail.com>
2024-02-07 23:15:23 +0530

    init

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    /* TODO the approach is to create a batch file-> this batch file compiles and executes a single testcase against an external cpp file we will execute the batch file using the system command -> the batch file uses another cpp program which fetches a testcase from the testcases file(this can be eliminated) then the output is shown to the user as a table with testcase its output and expected output */
+}
